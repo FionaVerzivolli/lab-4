bbfca7d61cb35a760383048a22463559ae304f59
AndrewSasmito <asasmito1920@gmail.com>
2024-09-30 16:27:51 -0400

    hi fone

--- src/main/java/app/gui/Application.java
+++ src/main/java/app/gui/Application.java
@@ -247,7 +247,7 @@ public class Application {
         // make a separate line.
         final JButton getAverageButton = new JButton("Get Average Grade");
         // TODO Task 4: Add another button for "Get Top Grade" (check the getAverageButton for example)
-
+        final JButton getTopButton = new JButton("Get Top Grade");
         final JButton leaveTeamButton = new JButton("Leave Team");
         final JLabel resultLabel = new JLabel();
 
@@ -265,6 +265,17 @@ public class Application {
         });
 
         // TODO Task 4: Add action listener for getTopGrade button, follow example of getAverageButton
+        getTopButton.addActionListener(event -> {
+            final String course = courseField.getText();
+            try {
+                final float grade = GetTopGradeUseCase.getTopGrade(course);
+                JOptionPane.showMessageDialog(jFrame, "Top grade: " + grade);
+                courseField.setText("");
+            }
+            catch (Exception ex) {
+                JOptionPane.showMessageDialog(jFrame, ex.getMessage());
+            }
+        });
 
         leaveTeamButton.addActionListener(event -> {
             try {
@@ -278,6 +289,7 @@ public class Application {
         theCard.add(new JLabel("The course you want to calculate the team average for:"));
         theCard.add(courseField);
         theCard.add(getAverageButton);
+        theCard.add(getTopButton);
         theCard.add(leaveTeamButton);
         theCard.add(resultLabel);
         return theCard;
